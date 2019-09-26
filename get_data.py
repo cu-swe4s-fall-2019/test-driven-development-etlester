3975a0118c26749773aa87458056a17bb3a6bdcd
Evan Lester <ETLester1@gmail.com>
2019-09-25 18:00:51 -0600

    get_data.py now conforms to pycodestyle. updated .travil.yml to test pycodestyle when running get_data.py

--- get_data.py
+++ get_data.py
@@ -8,32 +8,31 @@ def read_stdin_col(col_num):
     '''This function takes in an integer that references a
     column_number in standard input. it then selects the data in
     that column and returns the column data'''
-    #check to see if there is anything in stdin
-    if select.select([sys.stdin,],[],[],0.0)[0]:
+#    check to see if there is anything in stdin
+    if select.select([sys.stdin, ], [], [], 0.0)[0]:
         pass
     else:
         print('Error: There is no standard input', file=sys.stderr)
         sys.exit(1)
-
-    #read data from standard input
+#    #read data from standard input
     f = sys.stdin.read()
-    #split the data by return
+#    split the data by return
     ff = f.strip().split('\n')
-    #convert the data to a numpy array
+#   convert the data to a numpy array
     npff = np.array(ff)
     col_data = []
     for line in npff:
         type = line.split()
-        # print(type)
+
         try:
             working_col_data = int(type[col_num])
         except IndexError:
             print('Error: column number not in dataframe', file=sys.stderr)
             sys.exit(1)
         col_data.append(working_col_data)
-        # print(working_col_data)
-    # print(col_data)
+
     return col_data
+
 
 test_column = 1
 read_stdin_col(test_column)
